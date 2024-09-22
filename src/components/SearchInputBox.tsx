72dbfcfd6cb6ca737c48ed6f4b780077c92216fe
barkha-gupta <barkhagupta401@gmail.com>
2024-09-22 18:10:38 +0530

    feat: add dropdown component

--- src/components/SearchInputBox.tsx
+++ src/components/SearchInputBox.tsx
@@ -1,14 +1,34 @@
-import { FC, useContext } from "react";
+import { FC, useContext, useState } from "react";
 import { WeatherContext } from "../context/WeatherContext";
+import { City } from "country-state-city";
 
 const SearchInputBox: FC = () => {
+  const allCities = City.getAllCities();
+
   const { setCity, city } = useContext(WeatherContext);
+  const [firstSixMatching, setFirstSixMatching] = useState([]);
 
   const handleCityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCity(e.target.value);
+
+    if (e.target.value.length > 0) {
+      const filteredCities = allCities.filter((item) =>
+        item.name.toLowerCase().includes(e.target.value.toLowerCase())
+      );
+
+      setFirstSixMatching(filteredCities.slice(0, 6));
+    } else {
+      setFirstSixMatching([]);
+    }
   };
+
+  const handleCitySelect = (selectedCity: string) => {
+    setCity(selectedCity);
+    setFirstSixMatching([]); // Clear the dropdown list to make it disappear
+  };
+
   return (
-    <div>
+    <div className="search-container flex flex-col justify-center items-center">
       <input
         className="search-input rounded-full bg-secondary-text placeholder:text-primary-text focus:outline-none focus:border-primary-text focus:ring-black focus:ring-1 sm:text-sm border-shadow-custom"
         type="text"
@@ -16,6 +36,18 @@ const SearchInputBox: FC = () => {
         onChange={handleCityChange}
         value={city}
       />
+      <ul>
+        {firstSixMatching &&
+          firstSixMatching.map((item, index) => (
+            <li
+              key={index}
+              onClick={() => handleCitySelect(item.name)} // Handle city selection
+              className={city === item.name ? "active" : ""}
+            >
+              {item.name}
+            </li>
+          ))}
+      </ul>
     </div>
   );
 };
